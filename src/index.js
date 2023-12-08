bfb7ebbc4d8abbcd36730f6f919cc9d26daa6e6e
Sheralieva Bermet <bermetsheralieva79@gmail.com>
2023-12-08 19:26:14 +0600

    first commit

--- src/index.js
+++ src/index.js
@@ -4,18 +4,18 @@ import "./index.css";
 import App from "./App";
 import { Provider } from "react-redux";
 import store, { persistor } from "./store/store";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import { PersistGate } from "redux-persist/integration/react";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <PersistGate loading="loading.." persistor={persistor}>
         <Provider store={store}>
           <App />
         </Provider>
       </PersistGate>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
